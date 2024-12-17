78d9089c3e80087ae85bbadc27c750fdb75e5580
krishundre <krish.undre@somaiya.edu>
2024-12-17 13:28:09 +0530

    Made the testimonail page responsive and updated design on home page

--- project-code/src/Components/Homepage.jsx
+++ project-code/src/Components/Homepage.jsx
@@ -5,6 +5,19 @@ import TestimonialsCarousel from "./Testimonials";
 import anonymity from "../Assets/anonymity.jpg"
 import community from "../Assets/community.jpg"
 import reaction1 from "../Assets/reaction1.png"
+import { Tilt } from 'react-tilt'
+
+const defaultOptions = {
+  reverse: false,  // reverse the tilt direction
+  max: 35,     // max tilt rotation (degrees)
+  perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1,    // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000,   // Speed of the enter/exit transition
+  transition: true,   // Set a transition on enter/exit.
+  axis: null,   // What axis should be disabled. Can be X or Y.
+  reset: true,    // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
 
 const Homepage = () => {
   const { setLoading } = useLoading();
@@ -36,23 +49,29 @@ const Homepage = () => {
         <div className="row text-center">
           <div className="col-md-4">
             <div className="benefit-block">
-              <img src={anonymity} className="benefit-image w-50"></img>
-              <h3 className="benefit-title">Anonymity 🔶</h3>
-              <p className="benefit-description">Share advice without revealing your identity. 😐</p>
+              <Tilt options={defaultOptions}>
+                <img src={anonymity} className="benefit-image w-50"></img>
+                <h3 className="benefit-title">Anonymity 🔶</h3>
+                <p className="benefit-description">Share advice without revealing your identity. 😐</p>
+              </Tilt>
             </div>
           </div>
           <div className="col-md-4">
             <div className="benefit-block">
-            <img src={community} className="benefit-image w-50"></img>
-              <h3 className="benefit-title">Community 🌍</h3>
-              <p className="benefit-description">Join a diverse community of advice-givers and seekers. 🤝</p>
+              <Tilt options={defaultOptions}>
+                <img src={community} className="benefit-image w-50"></img>
+                <h3 className="benefit-title">Community 🌍</h3>
+                <p className="benefit-description">Join a diverse community of advice-givers and seekers. 🤝</p>
+              </Tilt>
             </div>
           </div>
           <div className="col-md-4">
             <div className="benefit-block">
-            <img src={reaction1} className="benefit-image w-50"></img>
-              <h3 className="benefit-title">Reactions ❤️👍</h3>
-              <p className="benefit-description">Get feedback with reactions to advice shared. 💬👁</p>
+              <Tilt options={defaultOptions}>
+                <img src={reaction1} className="benefit-image w-50"></img>
+                <h3 className="benefit-title">Reactions ❤️👍</h3>
+                <p className="benefit-description">Get feedback with reactions to advice shared. 💬👁</p>
+              </Tilt>
             </div>
           </div>
         </div>
